00d8f745a8ba7028738c7d357dcbcc5357553571
gliggy <leocfitz@gmail.com>
2022-11-09 15:21:05 -0500

    Create 1.1.1.java

---
+++ 1.1.1.java
@@ -0,0 +1,11 @@
+public class Program
+{
+    public static void main(String[] args)
+    {
+        int repeat = 0;
+        while (repeat < 10) {
+            System.out.println("Hello world!");
+            repeat++;
+        }
+    }
+}
